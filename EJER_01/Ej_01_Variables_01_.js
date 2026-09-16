9f42b6b25a3a9b8cab249264fb71d2f19ecd105c
jmariatf50 <jmariatf50@gmail.com>
2026-09-16 10:03:54 +0200

    Hola Puta

---
+++ EJER_01/Ej_01_Variables_01_.js
@@ -0,0 +1,13 @@
+//Declaro la variable Nombre tipo string y es constante no se puede modificar
+const nombre = "Jose Maria"
+
+//Declaro la variable edad como es let se puede modificar luego. Es de tipo int
+let edad = 21
+
+//Declaro la variable tieneMascota tipo boolean y no es modificables ya que es un const.
+const tieneMascota = true
+
+//Modifico la edad
+edad = 22
+
+console.timeLog(nombre)
